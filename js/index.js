8ee78a97d5e7c690d9d0401de0cca3f41c730c7a
sleedev <yeaji105@naver.com>
2023-06-30 17:04:43 +0900

    키보드 기능 구현
    1. 키보드 눌러서 입력 가능하도록 함
    2. 정답 확인 시 키보드에도 표시 되도록 함

--- js/index.js
+++ js/index.js
@@ -46,13 +46,20 @@ function appStart() {
       const inputText = block.innerText;
       const answerAlphabet = answer[i];
 
+      const keyBoard = document.querySelector(
+        `.key-block[data-key='${inputText}']`
+      );
+
       if (inputText === answerAlphabet) {
         answerNumber += 1;
         block.style.background = "#6AAA64";
+        keyBoard.style.background = "#6AAA64";
       } else if (answer.includes(inputText)) {
         block.style.background = "#C9B458";
+        keyBoard.style.background = "#C9B458";
       } else {
         block.style.background = "#787C7E";
+        keyBoard.style.background = "#787C7E";
       }
     }
 
@@ -61,21 +68,7 @@ function appStart() {
   };
 
   const handleKeydown = (event) => {
-    const key = event.key.toUpperCase();
-    const keyCode = event.keyCode;
-    const thisBlock = document.querySelector(
-      `.board-block[data-index='${attempts}${index}']`
-    );
-
-    if (event.key === "Backspace") handleBackspace();
-    if (index === 5) {
-      if (event.key === "Enter") {
-        handleEnterkey();
-      } else return;
-    } else if (65 <= keyCode && keyCode <= 90) {
-      thisBlock.innerHTML = key;
-      index += 1;
-    }
+    setValue(event);
   };
 
   const startTimer = () => {
@@ -94,8 +87,56 @@ function appStart() {
     timerId = setInterval(setTime, 1000);
   };
 
+  const handleClick = (event) => {
+    setValue(event);
+  };
+
+  const setValue = (event) => {
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+
+    const type = event.type;
+    const value =
+      type === "keydown" ? event.key.toUpperCase() : event.target.dataset.key;
+
+    if (value === "BACKSPACE") {
+      handleBackspace();
+      return;
+    }
+
+    if (index !== 5) {
+      if (value === "ENTER") {
+        return;
+      }
+
+      if (type === "keydown") {
+        const keyCode = event.keyCode;
+        if (65 <= keyCode && keyCode <= 90) {
+          thisBlock.innerHTML = value;
+          index += 1;
+        }
+      } else if (type === "click") {
+        thisBlock.innerHTML = value;
+        index += 1;
+      }
+    }
+
+    // 한 줄의 모든 칸이 입력 되었을 때
+    else {
+      if (value === "ENTER") {
+        handleEnterkey();
+      } else return;
+    }
+  };
+
   startTimer();
   window.addEventListener("keydown", handleKeydown);
+  let key = document.querySelectorAll(".key-block");
+
+  key.forEach((k) => {
+    k.addEventListener("click", handleClick);
+  });
 }
 
 appStart();
